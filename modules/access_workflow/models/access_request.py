83e926e7b0e28fb34b41282fbc119fed1bf0ec32
hiranasirhi <231289@students.au.edu.pk>
2026-02-23 23:35:08 -0500

    Implemented Access Workflow module

--- modules/access_workflow/models/access_request.py
+++ modules/access_workflow/models/access_request.py
@@ -5,10 +5,21 @@ class AccessRequest(db.Model):
     __tablename__ = "access_requests"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+
+    employee_id = db.Column(db.Integer, nullable=False)
     resource_id = db.Column(db.Integer, nullable=False)
 
-    status = db.Column(db.String(20), default="PENDING")
+    status = db.Column(db.String(30), default="PENDING")
+
+    manager_id = db.Column(db.Integer, nullable=False)
+    approved_by = db.Column(db.Integer)
+
     requested_at = db.Column(db.DateTime, default=datetime.utcnow)
-    reviewed_at = db.Column(db.DateTime)
-    reviewed_by = db.Column(db.Integer)
+    approved_at = db.Column(db.DateTime)
+
+    access_start = db.Column(db.DateTime)
+    access_end = db.Column(db.DateTime)
+
+    reason = db.Column(db.Text)
+
+    is_active = db.Column(db.Boolean, default=False)
